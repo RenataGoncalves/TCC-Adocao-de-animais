73b4dbf4f2068d6276515d1566a870ad4ca6c843
RenataGoncalves <renataagoncalves98@gmail.com>
2019-11-25 20:56:55 -0400

    estilização da pagina descrição

--- ongAdocao/adocao/views.py
+++ ongAdocao/adocao/views.py
@@ -5,12 +5,13 @@ from django.core.paginator import Paginator
 def descricao(request,idAnimal):
 	contexto = {}
 	animal = Animal.objects.get(id=idAnimal)
-	imagens = Imagens.objects.filter(animal=animal)
+	imagens = Imagens.objects.filter(animal=animal)[1:]
+	imagem1 = Imagens.objects.filter(animal=animal)[0]
 	contexto['animal'] = animal
 	contexto['imagens'] = imagens
+	contexto['imagem1'] = imagem1
 
 	return render(request, 'descricao.html',contexto)
-
 
 def index(request):
 	imagens = Imagens.objects.all()
